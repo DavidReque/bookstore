eac18cc30d1674b7a2088588bbf4a34699383c77
David Requeno <davidrequeno52@gmail.com>
2023-04-01 18:12:48 -0600

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Index from './pages/Index'
+import Create from './pages/create'
+import View from './pages/view'
 
 function App() {
 
   return (
-    <div className="App">
-      Hola
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Index />}/>
+        <Route path='create' element={<Create/>}/>
+        <Route path='view/:bookId' element={<View/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
