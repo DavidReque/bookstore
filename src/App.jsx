114f1dfff2f6bf1d3def17b6087e76c17600c1aa
David Requeno <davidrequeno52@gmail.com>
2023-04-04 19:15:15 -0600

    View

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import Create from "./pages/Create";
-import View from "./pages/view";
+import View from "./pages/View";
 import Store from "./store/store";
 
 function App() {
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,7 +1,32 @@
+import { useParams } from "react-router-dom";
 import Layaout from "../components/Layaout";
+import { useEffect, useState } from "react";
+import { useAppContext } from "../store/store";
 
 const View = () => {
-  return <Layaout>View</Layaout>;
+  const [item, setItem] = useState({});
+  const params = useParams();
+  const store = useAppContext();
+
+  useEffect(() => {
+    const book = store.getItem(params.bookId);
+    setItem(book);
+  }, []);
+
+  if (!item) {
+    return <Layaout>Item not found</Layaout>;
+  }
+
+  return (
+    <Layaout>
+      <h2>{item?.title}</h2>
+      <div>{item?.cover ? <img src={item?.cover} width="400" /> : ""}</div>
+      <div>{item?.author}</div>
+      <div>{item?.intro}</div>
+      <div>{item?.completed ? "Leido" : "Por terminar"}</div>
+      <div>{item?.review}</div>
+    </Layaout>
+  );
 };
 
 export default View;
