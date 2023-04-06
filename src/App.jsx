942b53769e0a0cb0f327554e5cabb794b88417ec
David Requeno <davidrequeno52@gmail.com>
2023-04-05 18:34:13 -0600

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import Create from "./pages/Create";
 import View from "./pages/View";
-import Store from "./store/store";
+import Store from "./store/store.jsx";
 
 function App() {
   return (
